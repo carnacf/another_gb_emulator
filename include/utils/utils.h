43879fe56950b34820d9eb27ba6343905d27377b
carnacf <florian.carnac@gmail.com>
2023-07-24 11:19:24 +0200

    Add bit manipulation functions into utils

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -21,4 +21,19 @@ FORCEINLINE inline uint16_t to16(uint8_t msb, uint8_t lsb)
 {
     return ((uint16_t)(msb) << 8) | (uint16_t)(lsb);
 }
+
+FORCEINLINE inline bool testBit(uint8_t val, uint8_t bit)
+{
+    return (val & (1 << bit)) != 0;
+}
+
+FORCEINLINE inline uint8_t setBit(uint8_t val, uint8_t bit)
+{
+    return val | (1 << bit);
+}
+
+FORCEINLINE inline uint8_t resetBit(uint8_t val, uint8_t bit)
+{
+    return val & ~(1 << bit);
+}
 }
