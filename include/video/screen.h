5aa6d949b25fafcdded5cd6b917712dc72e2b5fd
carnacf <florian.carnac@gmail.com>
2023-07-24 11:22:47 +0200

    Screen: management of ppu loop with management of stat register interrupts

--- include/video/screen.h
+++ include/video/screen.h
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 
+class Memory;
+
 namespace video
 {
 class Screen
@@ -10,10 +12,15 @@ public:
 
 	void tick(uint8_t count);
 
+	void setMemory(Memory* memory);
+
 	void setSCY(uint8_t scy);
 	void setSCX(uint8_t scx);
 	void setWY(uint8_t wy);
 	void setWX(uint8_t wx);
+
+	uint8_t getLY() const;
+	void setLYC(uint8_t lyc);
 
 	void enableLCD(bool enabled);
 	void enableWindow(bool enabled);
@@ -25,16 +32,27 @@ public:
 	void setBgTileMapAddr(uint16_t addr);
 
 private:
+
+	void updateStatusRegister();
+
+private:
+	Memory* m_memory;
+
 	uint16_t m_windowTileMapAddr = 0x9800;
 	uint16_t m_tileDataArea = 0x8800;
 	uint16_t m_bgTileMapAddr = 0x9800;
 	
 	uint8_t m_ObjectSize = 8;
 	
+	uint8_t m_ly = 0;
+	uint8_t m_lyc = 0;
+
 	uint8_t m_scy = 0;
 	uint8_t m_scx = 0;
 	uint8_t m_wy = 0;
 	uint8_t m_wx = 0;
+	
+	uint16_t m_scanlineCounter = 0;
 
 	bool m_objectEnable = false;
 	bool m_bgAndWindowPriority = false;
--- src/video/screen.cpp
+++ src/video/screen.cpp
@@ -1,10 +1,49 @@
 #include "video/screen.h"
 
+#include "memory/memory.h"
+#include "utils/utils.h"
+
 namespace video
 {
 void Screen::tick(uint8_t count)
 {
+	updateStatusRegister();
 
+	if (m_lcdEnabled)
+	{
+		m_scanlineCounter += count;
+	}
+	else
+	{
+		return;
+	}
+
+	if (m_scanlineCounter >= 456)
+	{
+		m_ly++;
+		m_scanlineCounter = 0;
+
+		if (m_ly == 144)
+		{
+			// Entering VBlank
+			uint8_t if_flag = m_memory->read8(0xFF0F);
+			if_flag = utils::setBit(if_flag, 0);
+			m_memory->write8(0xFF0F, if_flag);
+		}
+		else if (m_ly > 153)
+		{
+			m_ly = 0;
+		}
+		else if (m_ly < 144)
+		{
+			// Render ScanLine ?
+		}
+	}
+}
+
+void Screen::setMemory(Memory* memory)
+{
+	m_memory = memory;
 }
 
 void Screen::setSCY(uint8_t scy)
@@ -25,6 +64,16 @@ void Screen::setWY(uint8_t wy)
 void Screen::setWX(uint8_t wx)
 {
 	m_wx = wx;
+}
+
+uint8_t Screen::getLY() const
+{
+	return m_ly;
+}
+
+void Screen::setLYC(uint8_t lyc)
+{
+	m_lyc = lyc;
 }
 
 void Screen::enableLCD(bool enabled)
@@ -65,5 +114,80 @@ void Screen::setTileDataArea(uint16_t addr)
 void Screen::setBgTileMapAddr(uint16_t addr)
 {
 	m_bgTileMapAddr = addr;
+}
+
+void Screen::updateStatusRegister()
+{
+	uint8_t status = m_memory->read8(0xFF41);
+	if (!m_lcdEnabled)
+	{
+		m_scanlineCounter = 0;
+		m_ly = 0;
+
+		// Set mode to 1
+		status &= 0xFC;
+		status |= 1;
+		m_memory->write8(0xFF41, status);
+
+		return;
+	}
+
+	uint8_t currentMode = status & 0x03;
+	bool requestInterrupt = false;
+
+	// VBlank
+	if (m_ly >= 144)
+	{
+		status = utils::resetBit(status, 0);
+		status = utils::setBit(status, 1);
+		requestInterrupt = utils::testBit(status, 4);
+	}
+	else
+	{
+		if (m_scanlineCounter < 80)
+		{
+			// Mode 2
+			status = utils::setBit(status, 1);
+			status = utils::resetBit(status, 0);
+			requestInterrupt = utils::testBit(status, 5);
+		}
+		else if (m_scanlineCounter < 172)
+		{
+			// Mode 3
+			status = utils::setBit(status, 0);
+			status = utils::setBit(status, 1);
+		}
+		else
+		{
+			// Mode 0
+			status = utils::resetBit(status, 0);
+			status = utils::resetBit(status, 1);
+			requestInterrupt = utils::testBit(status, 3);
+		}
+	}
+
+	if (requestInterrupt && (currentMode != (status & 0x03)))
+	{
+		uint8_t if_flag = m_memory->read8(0xFF0F);
+		if_flag = utils::setBit(if_flag, 1);
+		m_memory->write8(0xFF0F, if_flag);
+	}
+
+	if (m_ly == m_lyc)
+	{
+		status = utils::setBit(status, 2);
+		if (utils::testBit(status, 6))
+		{
+			uint8_t if_flag = m_memory->read8(0xFF0F);
+			if_flag = utils::setBit(if_flag, 1);
+			m_memory->write8(0xFF0F, if_flag);
+		}
+	}
+	else
+	{
+		status = utils::resetBit(status, 2);
+	}
+
+	m_memory->write8(0xFF41, status);
 }
 }
