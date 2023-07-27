2af3ce42fa459a9e1e630ddedf736cb24a04e08a
carnacf <florian.carnac@gmail.com>
2023-07-28 00:20:06 +0200

    Scanline rendering for background

--- include/video/screen.h
+++ include/video/screen.h
@@ -6,13 +6,21 @@ class Memory;
 
 namespace video
 {
+constexpr uint8_t SCREEN_WIDTH = 160;
+constexpr uint8_t SCREEN_HEIGHT = 140;
+
 class Screen
 {
 public:
 
+	Screen();
+	~Screen();
+
 	void tick(uint8_t count);
 
 	void setMemory(Memory* memory);
+
+	uint8_t* getFrameBuffer();
 
 	void setSCY(uint8_t scy);
 	void setSCX(uint8_t scx);
@@ -30,10 +38,21 @@ public:
 	void setWindowTileMapAddr(uint16_t addr);
 	void setTileDataArea(uint16_t addr);
 	void setBgTileMapAddr(uint16_t addr);
+	
+	uint8_t getBGPalette() const;
+	void setBGPalette(uint8_t bgPalette);
 
 private:
 
 	void updateStatusRegister();
+
+	void renderBG(uint8_t line);
+	void renderWindow(uint8_t line);
+	void renderObjects(uint8_t line);
+
+	uint64_t getTileLine(uint16_t tileAddr, uint8_t line);
+	uint16_t fromTileIdtoAdress(uint8_t tileId);
+	uint8_t fromColorIdtoColor(uint8_t colorId);
 
 private:
 	Memory* m_memory;
@@ -41,6 +60,8 @@ private:
 	uint16_t m_windowTileMapAddr = 0x9800;
 	uint16_t m_tileDataArea = 0x8800;
 	uint16_t m_bgTileMapAddr = 0x9800;
+	
+	uint8_t* m_frameBuffer = nullptr;
 	
 	uint8_t m_ObjectSize = 8;
 	
@@ -52,6 +73,8 @@ private:
 	uint8_t m_wy = 0;
 	uint8_t m_wx = 0;
 	
+	uint8_t m_bgPalette = 0;
+
 	uint16_t m_scanlineCounter = 0;
 
 	bool m_objectEnable = false;
--- src/video/screen.cpp
+++ src/video/screen.cpp
@@ -5,6 +5,20 @@
 
 namespace video
 {
+Screen::Screen()
+	: m_frameBuffer(new uint8_t[SCREEN_WIDTH * SCREEN_HEIGHT * 4]())
+{
+	for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT * 4; i++)
+	{
+		m_frameBuffer[i] = 255;
+	}
+}
+
+Screen::~Screen()
+{
+	delete[] m_frameBuffer;
+}
+	
 void Screen::tick(uint8_t count)
 {
 	updateStatusRegister();
@@ -37,6 +51,7 @@ void Screen::tick(uint8_t count)
 		else if (m_ly < 144)
 		{
 			// Render ScanLine ?
+			renderBG(m_ly);
 		}
 	}
 }
@@ -44,6 +59,11 @@ void Screen::tick(uint8_t count)
 void Screen::setMemory(Memory* memory)
 {
 	m_memory = memory;
+}
+
+uint8_t* Screen::getFrameBuffer()
+{
+	return m_frameBuffer;
 }
 
 void Screen::setSCY(uint8_t scy)
@@ -114,6 +134,16 @@ void Screen::setTileDataArea(uint16_t addr)
 void Screen::setBgTileMapAddr(uint16_t addr)
 {
 	m_bgTileMapAddr = addr;
+}
+
+uint8_t Screen::getBGPalette() const
+{
+	return m_bgPalette;
+}
+
+void Screen::setBGPalette(uint8_t bgPalette)
+{
+	m_bgPalette = bgPalette;
 }
 
 void Screen::updateStatusRegister()
@@ -190,4 +220,66 @@ void Screen::updateStatusRegister()
 
 	m_memory->write8(0xFF41, status);
 }
+
+void Screen::renderBG(uint8_t line)
+{
+	uint16_t tileMapY = m_scy / 8 + line / 8;
+	uint16_t startAddr = m_bgTileMapAddr + tileMapY * 32;
+
+	for (uint8_t tileY = 0; tileY < 8; tileY++)
+	{
+		for (uint8_t x = 0; x < SCREEN_WIDTH; x++)
+		{
+			uint8_t tileMapX = (x + m_scx) / 8;
+			uint8_t tilePixel = (x + m_scx) % 8;
+
+			uint16_t tileIdAddress = startAddr + tileMapX;
+			uint8_t tileId = m_memory->read8(tileIdAddress);
+			uint16_t tileAdress = fromTileIdtoAdress(tileId);
+
+			uint64_t tileLine = getTileLine(tileAdress, tileY);
+
+			uint8_t colorId = ((uint8_t*)&tileLine)[tilePixel];
+			m_frameBuffer[line * SCREEN_WIDTH * 4 + x * 4] = 255 - fromColorIdtoColor(colorId);
+			m_frameBuffer[line * SCREEN_WIDTH * 4 + x * 4 + 1] = 255 - fromColorIdtoColor(colorId);
+			m_frameBuffer[line * SCREEN_WIDTH * 4 + x * 4 + 2] = 255 - fromColorIdtoColor(colorId);
+			m_frameBuffer[line * SCREEN_WIDTH * 4 + x * 4 + 3] = 255;
+		}
+	}
+}
+uint64_t Screen::getTileLine(uint16_t tileAddr, uint8_t line)
+{
+	// We pair the two Bytes of a given line into pairs of bits store in a 8 byte value
+	// This is an trick to easily get the pixels colors.
+	uint64_t a = m_memory->read8(tileAddr + line * 2);
+	uint64_t tmpA = a * 0x0100040010004001 & 0x0001000100010001;
+	uint64_t tmpB = a * 0x0002000800200080 & 0x0100010001000100;
+	a = tmpA | tmpB;
+
+	uint64_t b = m_memory->read8(tileAddr + line * 2 + 1);
+	tmpA = b * 0x0100040010004001 & 0x0001000100010001;
+	tmpB = b * 0x0002000800200080 & 0x0100010001000100;
+	b = (tmpA | tmpB) << 1;
+
+	return a | b;
+}
+
+uint16_t Screen::fromTileIdtoAdress(uint8_t tileId)
+{
+	uint8_t tileMemorySize = 16;
+	switch (m_tileDataArea)
+	{
+	case 0x8800:
+		return m_tileDataArea + (tileId + 128) * tileMemorySize;
+	case 0x8000:
+	default:
+		return m_tileDataArea + tileId * tileMemorySize;
+	}
+}
+
+uint8_t Screen::fromColorIdtoColor(uint8_t colorId)
+{
+	return  m_bgPalette & (0b0000'0011 << (colorId*2));
+}
+
 }
